c5199827afd8edb0298ae27e906b35cdcf814796
Kyle <kjbenk@gmail.com>
2016-03-01 08:24:33 -0500

    Added Endpoint for single site option
    * ADDED: Single site option endpoint
    * ADDED: Single site option read and update actions

--- tests/test-rest-site-controller.php
+++ tests/test-rest-site-controller.php
@@ -11,6 +11,11 @@ class WP_Test_REST_Site_Controller extends WP_Test_REST_Controller_Testcase {
 	public function setUp() {
 		parent::setUp();
 
+		$this->user = $this->factory->user->create( array(
+			'role' => 'administrator',
+		) );
+		wp_set_current_user( $this->user );
+
 		$this->endpoint = new WP_REST_Site_Controller();
 	}
 
@@ -28,27 +33,43 @@ class WP_Test_REST_Site_Controller extends WP_Test_REST_Controller_Testcase {
 	}
 
 	public function test_get_items() {
-		$request = new WP_REST_Request( 'GET', '/wp/v2/users' );
-		$request->set_param( 'context', 'view' );
+		$request = new WP_REST_Request( 'GET', '/wp/v2/site' );
 		$response = $this->server->dispatch( $request );
+		$data = $response->get_data();
 
 		$this->assertEquals( 200, $response->get_status() );
+		$this->assertCount( 13, $data );
 	}
 
 	public function test_get_item() {
+		$request = new WP_REST_Request( 'GET', '/wp/v2/site/title' );
+		$response = $this->server->dispatch( $request );
+		$data = $response->get_data();
 
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertNotEmpty( $data );
 	}
 
 	public function test_create_item() {
-
+		// No op
 	}
 
 	public function test_update_item() {
+		update_option( 'blogname', 'Old Title' );
 
+		$_POST['title'] = 'New Title';
+
+		$request = new WP_REST_Request( 'PUT', '/wp/v2/site/title' );
+		$request->set_body_params( $_POST );
+		$response = $this->server->dispatch( $request );
+		$data = $response->get_data();
+
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( 'New Title', $data['title'] );
 	}
 
 	public function test_delete_item() {
-
+		// No op
 	}
 
 	public function test_prepare_item() {
